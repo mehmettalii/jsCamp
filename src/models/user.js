9ec15484430cd70b449a244bb26f77a0adcb0f76
mehmettalii <84227393+mehmettalii@users.noreply.github.com>
2021-11-27 19:07:00 +0300

    update

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,8 @@
-export default class User {
-  constructor(id, firstName, lastName, city) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.city = city;
-  }
-}
+// export default class User {
+//   constructor(id, firstName, lastName, city) {
+//     this.id = id;
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.city = city;
+//   }
+// }
