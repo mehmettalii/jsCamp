bbb54b8ca925f9ef2563b3419841c5b5ab56d7e5
mehmettalii <84227393+mehmettalii@users.noreply.github.com>
2021-11-27 19:08:09 +0300

    update

--- src/homework.js
+++ src/homework.js
@@ -1,25 +1,25 @@
 // // Homework (week-2)
 
-// // 1- JavaScript ile istediğiniz kadar sayı parametre gönderebileceğiniz bir fonksiyon yazınız. Bu fonksiyona gönderdiğiniz her sayı için çıktı olarak asal olup olmadığını yazınız.
+// 1- JavaScript ile istediğiniz kadar sayı parametre gönderebileceğiniz bir fonksiyon yazınız. Bu fonksiyona gönderdiğiniz her sayı için çıktı olarak asal olup olmadığını yazınız.
 
-// function findPrime(...numbers) {
-//     let Numbers = numbers;
-//     let primeNumber = [];
-//     let count;
-//     for (let i = 0; i < Numbers.length; i++) {
-//       count = 0;
-//       for (let j = 2; j < Numbers[i]; j++) {
-//         if (Numbers[i] % j == 0) {
-//           count++;
-//         }
-//       }
-//       if (count == 0) {
-//         primeNumber.push(Numbers[i]);
-//       }
-//     }
-//     console.log(primeNumber);
-//   }
-//   findPrime(3, 45, 81, 16, 7, 19, 17, 8, 5, 4);
+function findPrime(...numbers) {
+    let Numbers = numbers;
+    let primeNumber = [];
+    let count;
+    for (let i = 0; i < Numbers.length; i++) {
+      count = 0;
+      for (let j = 2; j < Numbers[i]; j++) {
+        if (Numbers[i] % j == 0) {
+          count++;
+        }
+      }
+      if (count == 0) {
+        primeNumber.push(Numbers[i]);
+      }
+    }
+    console.log(primeNumber);
+  }
+  findPrime(3, 45, 81, 16, 7, 19, 17, 8, 5, 4);
 
 //   // 2- Parametre olarak girilen iki sayının arkadaş sayılar olup olmadığını bulan programı yazınız.
 
